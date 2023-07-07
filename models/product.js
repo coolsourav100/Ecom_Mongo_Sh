f1ec727ade1b8dc20b78d9ff96c36bb16df9f9ca
coolsourav100 <sarkar.sourav26jan@gmail.com>
2023-07-08 00:15:56 +0530

    crud operation done

--- models/product.js
+++ models/product.js
@@ -1,19 +1,57 @@
+const mongodb = require('mongodb')
 const getDb = require('../util/database').getDb
 
 class Product{
-  constructor(title,price,description,imgUrl){
+  constructor(title,price,description,imgUrl,id){
     this.title = title
     this.price = price
     this.description = description
     this.imgUrl = imgUrl
+    this._id = new mongodb.ObjectId(id)
   }
   save(){
 let db= getDb()
-return db.collection('products').insertOne(this).then((res)=>{
+let dpOp
+if(this._id){
+  dpOp =db.collection('products').updateOne({_id: new mongodb.ObjectId(this._id)},{$set:this})
+}else{
+dpOp = db.collection('products').insertOne(this)
+}
+return dpOp.then((res)=>{
   console.log(res)
 }).catch(err=>{
   console.log(err)
 })
+  }
+  static fetchAll(){
+    let db = getDb()
+    return db.collection('products').find().toArray()
+    .then((res)=>{
+      console.log(res)
+      return res
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+
+  static findById(prodId){
+    let db = getDb()
+    return db.collection('products').find({_id: new mongodb.ObjectId(prodId)}).next()
+    .then((res)=>{
+      return res
+    })
+    .catch(err=>console.log(err))
+  }
+
+  static delete(prodId){
+    let db = getDb()
+    return db.collection('products').deleteOne({_id:new  mongodb.ObjectId(prodId)}).then((res)=>{
+      console.log(res)
+      return res
+    }).catch(err=>{
+      console.log(err)
+    })
   }
 }
 
